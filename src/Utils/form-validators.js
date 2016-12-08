fe609aefd8809c33761772115b423b2438a1971f
Mathan Appadurai <mathan.a@flipkart.com>
2016-12-08 19:54:12 +0530

    Adds number and decimal validator

--- src/Utils/form-validators.js
+++ src/Utils/form-validators.js
@@ -1,3 +1,6 @@
+const NUMBER_ONLY_REGEXP = /^\d+$/;
+const DECIMAL_REGEXP = /^[0-9]+(\.[0-9]{1,2})?$/;
+
 function required (arg) {
   if (arg) arg = arg.trim();
   if (arg) return {valid: true, error: ''};
@@ -5,6 +8,20 @@ function required (arg) {
   return {valid: false, error: 'Field cannot be empty or spaces.'};
 }
 
+function number (arg = '') {
+	if(NUMBER_ONLY_REGEXP.test(arg)) return {valid: true, error: ''};
+
+	return {valid: false, error: 'Only numbers are allowed'};
+}
+
+function decimal (arg = '') {
+	if(DECIMAL_REGEXP.test(arg)) return {valid: true, error: ''};
+
+	return {valid: false, error: 'Invalid value'};
+}
+
 export default {
-  required
+  required,
+	number,
+	decimal
 };
